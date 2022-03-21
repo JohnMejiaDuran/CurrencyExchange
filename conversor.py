aa8c795929971d0e3dd1b2f7bcf6f12d176dfeac
John <johnmejiaduran@gmail.com>
2022-03-21 17:11:24 -0500

    Add MXN pesos

--- conversor.py
+++ conversor.py
@@ -5,28 +5,34 @@ Choose one option
 
 1. USD to COP
 2. COP to USD
+3. USD to MXN
+4. MXN to USD
 
 """
 
 def converter(money,dolar):
 
-    coin = float(input(f'How many {money} have you?: '))
-    if money == 'Dollars':
-        result=dolar*coin
-        result=str(result)
-        print (f'You have {result} Colombian Pesos')
+    coin = float(input(f'How many money have you?: '))
+    if option % 2 !=0:
+        result=coin*dolar
+        result=str(round(result,2))
+        print (f'You have {result} {money}')
     else:
         result=coin/dolar
-        result=round(result,2)
-        print (f'You have {result} Dollars')
+        result=str(round(result,2))
+        print (f'You have {result} {money}')
 
 option =int(input(menu))
 if option ==1:
-    converter('Dollars',3815)
+    converter('Colombian pesos',3815)
 elif option ==2:
-    converter('Colombian Pesos',3815)
+    converter('Colombian pesos',3815)
+elif option ==3:
+    converter('Mexican pesos', 20.37)
+elif option ==4:
+    converter('Mexican pesos', 20.37)
 else:
-    print (input(menu))
+    print('Escribe una opción correcta')
 
 if '__name__'=='__main__':
     converter()
